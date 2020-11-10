1f9b8126e520823a1cb03c26422c0987cb5a1b4b
Hans Rasmussen <angdeclo@gmail.com>
2020-11-10 15:46:05 +0100

    Started defining pins, added pinout, wrote a little README.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # temperature-controlled-box
 A control system to control the temperature in a heat isolated box.
+The box is meant to house a resin printer and keep it warm even though it's placed in a cold environment where its toxic fumes don't spread into the house.
+
+The IDE used for writing this code is Visual Studio Code with the PlatformIO add-on. This allows me to write C++ code and deploy it to the Arduino board with ease, and I'm even able to use the simplified Arduino functions and libraries that usually wouldn't be compatible with pure C++.
+
+### Components needed:
+1. An isolated box, in this case with a wooden frame with a double layered plexiglass front panel and flamingo inside.
+2. A micro controller, such as the arduino Uno or nano.
+3. A heating unit, in this case a .... with integrated fan.
+4. Some sensors
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,6 +2,11 @@
  * Heat Regulated Electric Oven 
  * 
  * This code works as a controller for box that is to keep its internal temperature stable.
+ * 
+ * Author:        Hans Rasmussen
+ * E-Mail:        angdeclo@gmail.com
+ * Creation Date: 09/10/2020
+ * Last Modified: 10/10/2020
  */
 
 
@@ -10,6 +15,27 @@
 // ##              Includes                ##
 // ##########################################
 #include <Arduino.h>
+
+
+
+// ##########################################
+// ##              Defines                 ##
+// ##########################################
+/*
+ * This section defines the settings for the program, such as pin numbers and operation modi.
+*/
+
+// Should the code be compiled with additional outputs? 0=off, 1=normal
+#define DEBUGMODE 0
+
+// Pin for onewire interface. The sensors are installed in series running without parasitic power.
+#define ONEWIRE_PIN 10
+
+// Pin on which to run the PWM for the motor controller in charge of the fan. The H-bridge is hard-wired.
+#define FAN_PWM 11
+
+// Pin controlling the relay in charge of the heating element.
+#define HEAT_RELAY_PIN 8
 
 
 
@@ -31,7 +57,7 @@ void setup()
 // ##                 Main                 ##
 // ##########################################
 /*
- * The loop function subsitudes the while(1)-function.
+ * The loop function subsitudes the while(1){} function.
 */
 void loop()
 {
