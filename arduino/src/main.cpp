2a8788b51b15136c3ac1ceef511c64dddd909fbb
Hans Rasmussen <angdeclo@gmail.com>
2021-03-25 23:45:55 +0100

    Reverted PID back to more simple model. Added comments.

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,12 +1,12 @@
 /*
- * Heat Regulated Electric Oven 
+ * Heat Regulated Box 
  * 
  * This code works as a controller for box that is to keep its internal temperature stable.
  * 
  * Author:        Hans Rasmussen
  * E-Mail:        angdeclo@gmail.com
  * Creation Date: 09/10/2020
- * Last Modified: 10/10/2020
+ * Last Modified: 25/03/2021
  */
 
 
@@ -28,6 +28,12 @@
  * This section defines the settings for the program, such as pin numbers and operation modi.
 */
 
+// Desired temperature in degrees celcius
+const double DESIRED_TEMPERATURE = 27.5;
+
+// How much deviation (+-DESIRED_TEMPERATURE) is allowed before correcting.
+const double TEMPERATURE_DEVIATION = 0.2;
+
 // Pin for onewire interface. The sensors are installed in series running without parasitic power.
 #define ONE_WIRE_BUS 10
 #define TEMPERATURE_PRECISION 12
@@ -37,14 +43,6 @@
 
 // Pin controlling the relay in charge of the heating element.
 #define HEAT_RELAY_PIN 11
-
-// PID values
-#define PID_KP 10;
-#define PID_KI 0.5;
-#define PID_KD 0.1;
-
-// PID analogue to digital period time (ms)
-#define PID_PERIOD 30000;
 
 
 // ##########################################
@@ -59,9 +57,6 @@ DallasTemperature sensors(&oneWire);
 DeviceAddress sensor0 = {0x28, 0x69, 0xA1, 0x69, 0x35, 0x19, 0x01, 0x5D};
 DeviceAddress sensor1 = {0x28, 0xD5, 0x55, 0x6B, 0x35, 0x19, 0x01, 0x99};
 DeviceAddress sensor2 = {0x28, 0x96, 0x5F, 0xE3, 0x22, 0x20, 0x01, 0x62};
-
-// Function to update the PID controller
-double computePID(double input);
 
 // Function that takes over the main loop and gives full control of the device.
 void debugMode();
@@ -86,23 +81,8 @@ uint8_t findDevices(int pin);
 /*
  * This section contains the global variables. It would be a good idea to change these global variables to local ones in the future, but they're here now.
 */
-bool relay_state = false;
-unsigned long LastLog = 0;
-int fanDutyCycle = 0;
 
-//PID constants
-unsigned long currentTime, previousTime;
-double elapsedTime;
-double error;
-double lastError;
-double input, output, setPoint;
-double cumError, rateError;
-
-// PID analogue to digital output
-unsigned long LastPeriod = 0;
-const int period = PID_PERIOD;
-int dutyCycle = 0;
-
+bool relay_state = false;   // Bool which saves the current state of the relay, mostly for logging purposes.
 
 
 // ##########################################
@@ -112,6 +92,7 @@ void setup()
 {
   // Initializations //
 
+  // Delay to allow us to turn on any logging scripts that we might have.
   delay(5000);
 
   // ==== Serial ====
@@ -156,7 +137,7 @@ void setup()
   Serial.println();
   Serial.print("Device 1 Description: Outside, reference\n");
 
-  // set the resolution to 9 bit per device
+  // set the resolution to 9 bit per device, this results in a 93.75ms reading time per sensor
   sensors.setResolution(sensor0, TEMPERATURE_PRECISION);
   sensors.setResolution(sensor1, TEMPERATURE_PRECISION);
   sensors.setResolution(sensor2, TEMPERATURE_PRECISION);
@@ -183,16 +164,13 @@ void setup()
   // Initialize IO pin as output
   pinMode(HEAT_RELAY_PIN, OUTPUT);
 
+  // Give advice on how to enter debugging more, and wait a second so the user will have chance to read it.
   Serial.println("For debugMode please press anykey.");
   Serial.println();
   delay(1000);
 
-  fanDutyCycle = 220;
-  analogWrite(FAN_PWM, fanDutyCycle);
-
-  // ==== PID Controller ====
-  // Set initial setpoint to 0
-  setPoint = 27; 
+  // Turn on the fan. For now we'll jsut keep it constant.
+  analogWrite(FAN_PWM, 220);
 
 }
 
@@ -203,70 +181,64 @@ void setup()
 // ##########################################
 void loop()
 {
+  // Check if the user wants to enter debugmode
   if (Serial.available() > 0)
   {
      debugMode();
   }
 
+  // Read all the sensors, and find the mean of the inside sensors.
+  sensors.requestTemperatures();
   float tempC_S0 = sensors.getTempC(sensor0);
   float tempC_S1 = sensors.getTempC(sensor1);
   float tempC_S2 = sensors.getTempC(sensor2);
   float tempC_mean = ((tempC_S0+tempC_S1)/2);
   
-  // Recompute the PID dutycycle once the period has complete
-  if ((millis() - LastPeriod) >= period)
+  // This is the actual control algorithm. 
+  // For now it simply turns the heating element on when the temperature falls below a deviation defined by the user, 
+  // and turns it off again once it surpasses this same deviation
+  if (tempC_mean <= (DESIRED_TEMPERATURE - TEMPERATURE_DEVIATION))
   {
-    sensors.requestTemperatures();
-    tempC_S0 = sensors.getTempC(sensor0);
-    tempC_S1 = sensors.getTempC(sensor1);
-    tempC_S2 = sensors.getTempC(sensor2);
-    tempC_mean = ((tempC_S0+tempC_S1)/2);
-
-    dutyCycle = computePID(tempC_mean);
+    digitalWrite(HEAT_RELAY_PIN, HIGH);
     relay_state = true;
-    LastPeriod = millis();
-
-    Serial.print("Current duty cycle: ");
-    Serial.println(dutyCycle);
-    Serial.println();
   }
-
-  // Turn off the relay once we reach the end of the active state of the duty cycle
-  if ((millis() - LastPeriod) >= ((period/100) * dutyCycle) && (relay_state == true))
+  else if (tempC_mean >= (DESIRED_TEMPERATURE + TEMPERATURE_DEVIATION))
   {
+    digitalWrite(HEAT_RELAY_PIN, LOW);
     relay_state = false;
   }
 
-  // Change the state of the heat relay
-  if (relay_state == true)
+  // Debugging and logging - Creates a JSON document and sends it over Serial
+  // This piece of code is mostly autogenerated using the ArduinoJson Assistant: https://arduinojson.org/v6/assistant/
+  // The following piece of JSON is used in step 2:
+  /*
   {
-    digitalWrite(HEAT_RELAY_PIN, HIGH);
-  }
-  else
+  "ms": 1000000,
+  "sensors": [
   {
-    digitalWrite(HEAT_RELAY_PIN, LOW);
+    "sensor01": 00.000,
+    "sensor02": 00.000,
+    "sensor03": 00.000
   }
-  
-
-  // Debugging and logging - Create a JSON document and send it over Serial
-  if ((millis() - LastLog)> 200)
-  {
-    StaticJsonDocument<96> doc;
-
-    doc["ms"] = millis();
-    JsonObject sensors_0 = doc["sensors"].createNestedObject();
-    sensors_0["sensor00"] = tempC_S0;
-    sensors_0["sensor01"] = tempC_S1;
-    sensors_0["sensor02"] = tempC_S2;
-    doc["sensorMean"] = tempC_mean;
-    doc["setpoint"] = setPoint;
-    doc["dutycycle"] = dutyCycle;
-    doc["fan"] = fanDutyCycle;
-    doc["heatingElement"] = relay_state;
-
-    serializeJson(doc, Serial);
-    Serial.println();
+  ],
+  "sensorMean": 00.000,
+  "fan": 255,
+  "heatingElement": 0
   }
+  */
+  StaticJsonDocument<96> doc;
+
+  doc["ms"] = millis();
+  JsonObject sensors_0 = doc["sensors"].createNestedObject();
+  sensors_0["sensor00"] = tempC_S0;
+  sensors_0["sensor01"] = tempC_S1;
+  sensors_0["sensor02"] = tempC_S2;
+  doc["sensorMean"] = tempC_mean;
+  doc["fan"] = 255;
+  doc["heatingElement"] = relay_state;
+
+  serializeJson(doc, Serial);
+  Serial.println();
 }
 
 
@@ -275,61 +247,19 @@ void loop()
 // ##               Functions              ##
 // ##########################################
 
-// Function to update the PID controller
-double computePID(double input){
-  double kp = PID_KP;
-  double ki = PID_KI;
-  double kd = PID_KD;    
-
-  currentTime = millis();                                 //get current time
-  elapsedTime = (double)(currentTime - previousTime);     //compute time elapsed from previous computation
-
-  error = setPoint - input;                               // determine error
-  cumError += error * (elapsedTime / 1000);                        // compute integral
-  rateError = (error - lastError)/(elapsedTime / 1000);            // compute derivative
-
-  lastError = error;                                      //remember current error
-  previousTime = currentTime;                             //remember current time
-
-  double output = kp*error + ki*cumError + kd*rateError;
-
-  // Make sure that the output does not exceed some extreme values
-  if (output > 1000)
-  {
-    output = 1000;
-  }
-  else if (output < 0)
-  {
-    output = 0;
-  }
-
-  // Normailize the output to be in promille
-  output = map(output, 0, 1000, 0, 100);
-  
-  Serial.print("kp*error: ");
-  Serial.println(kp*error);
-  Serial.print("ki*cumError: ");
-  Serial.println(ki*cumError);
-  Serial.print("kd*rateError: ");
-  Serial.println(kd*rateError);
-  Serial.print("output: ");
-  Serial.println(output);
-
-  return output;
-}
-
-
 // Function that takes over the main loop and gives full control of the device.
 void debugMode()
 {
   int debugmode_enabled = true;
   char rx_byte = 0;
 
+  // Clear any input we might have
   if (Serial.available() > 0) 
   {
     rx_byte = Serial.read();
   }
 
+  // Print the table of choices, read the input, and decide on what to do
   while (debugmode_enabled)
   {
     Serial.println("\nCommands:\n");
@@ -341,18 +271,20 @@ void debugMode()
     Serial.println("\t0 - Exit debugMode and return to normal operation\n");
     Serial.println("Choice: ");
     
+    // Wait for user input
     while(Serial.available() == 0)
     {
       delay(100);
     }
 
-    if (Serial.available() > 0) 
-    {    // is a character available?
-      rx_byte = Serial.read();       // get the character
+    // Once the user has sent a command, read and save it. This does not actually need an if-stament, but I like making safeties.
+    if (Serial.available() > 0)     // is a character available?
+    {    
+      rx_byte = Serial.read();      // get the character
       Serial.print(rx_byte);
       Serial.print("\n");
 
-      // check if a number was received
+      // check whether the recieved byte was valid, and execute the command
       if (rx_byte == '1')
       {
         Serial.println("\nReading temperature sensors. Press anykey to return.");
@@ -365,10 +297,11 @@ void debugMode()
           printData(sensor2);
           Serial.println();
 
+          // Checks for a character so that we can break free of this while loop
           char rx_byte_alt = 0;
-          if (Serial.available() > 0) 
-          {    // is a character available?
-            rx_byte_alt = Serial.read();       // get the character
+          if (Serial.available() > 0)         // is a character available?
+          {    
+            rx_byte_alt = Serial.read();      // get the character
             read_temp = 0;
           }
         }
@@ -383,9 +316,9 @@ void debugMode()
 
         while (read_str)
         {
-          if (Serial.available() > 0) 
-          {    // is a character available?
-            rx_byte_alt = Serial.read();       // get the character
+          if (Serial.available() > 0)         // is a character available?
+          {    
+            rx_byte_alt = Serial.read();      // get the character
             
             if (rx_byte_alt != '\n') 
             {
@@ -433,6 +366,7 @@ void debugMode()
   }
 
 }
+
 
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
@@ -504,6 +438,5 @@ uint8_t findDevices(int pin)
     Serial.print("// nr devices found: ");
     Serial.println(count);
   }
-
   return count;
 }
